a918fe2603d96869b298c514fed81e61dca76109
Joachim Goennheimer <Joachim.goennheimer@ibm.com>
2020-05-28 08:36:50 +0200

    implement quiz functionality

--- QuizSUI/ContentView.swift
+++ QuizSUI/ContentView.swift
@@ -8,23 +8,52 @@
 
 import SwiftUI
 
+let questions: [String] = [
+    "Whats i 6*7?",
+    "How much wood would a woodchuck chuck?",
+    "What is brown and sticky?"
+]
+
+let answers: [String] = [
+    "42. The Answer to life & universe & everything.",
+    "As much as he could",
+    "A stick."
+]
+
+var currentQuestionIndex: Int = 0
+let hideAnswer = "???"
+
 struct ContentView: View {
+    
+    @State var question: String
+    @State var answer: String
+    
     var body: some View {
         VStack (spacing: 20.0) {
-            Text("???")
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Text(question)
+            Button(action: {self.showNextQuestion()}) {
                 Text("Next Question")
             }
-            Text("???")
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Text(answer)
+            Button(action: {self.answer = answers[currentQuestionIndex]}) {
                 Text("Show Answer")
             }
         }
+    }
+    
+    func showNextQuestion() {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        question = questions[currentQuestionIndex]
+        answer = hideAnswer
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(question: questions[0], answer: "???")
     }
 }
